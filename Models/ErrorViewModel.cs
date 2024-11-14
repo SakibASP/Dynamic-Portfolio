1196f4204423580f2b49e9c1bfad68ab24d33e5e
Md. Sakibur Rahman <130847090+SakibASP@users.noreply.github.com>
2024-11-14 23:43:23 +0600

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SAKIB_PORTFOLIO.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
