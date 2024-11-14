b790f71aba6f010b3e7673998e453d93993bc942
sakibur.rahman.cse <sakibur.rahman.cse@gmail.com>
2024-11-15 05:22:21 +0600

    email password removing

--- Portfolio.Utils/EmailSettings.cs
+++ Portfolio.Utils/EmailSettings.cs
@@ -5,7 +5,7 @@
         public static string MailFromAddress = "sakibur.rahman.cse@gmail.com";
         public static bool UseSsl = true;
         public static string Username = MailFromAddress;
-        public static string Password = "kerksdaxnunmcotc";
+        public static string Password = "your app passsword from google";
         public static string ServerName = "smtp.gmail.com";
         public static int ServerPort = 587;
     }
